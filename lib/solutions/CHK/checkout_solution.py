dcc2e3804315c776a4650e9123dfafa75ab09b27
sbx_user1051 <sbx_user1051@169.254.126.177>
2019-06-19 08:05:02 +0000

    Wed Jun 19 08:05:02 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,12 +1,39 @@
-
+VALID_SKUS = ["A", "B", "C", "D"]
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    """Assuming skus is a continuous string, not comma separated"""
     if not isinstance(skus, str):
         return -1
 
+    # sort the skus, convert to list
+    sku_list = sorted(skus)
+    
+    # remove any blank entries
+    sku_list = [i for i in sku_list if i.strip() != ""]
+
+    # validate skus
+    sku_list = [i.upper() for i in sku_list if i.upper() in VALID_SKUS]
+
+    # count skus
+    sku_data = dict(A=0, B=0, C=0, D=0)
+    for sku in sku_list:
+        sku_data[sku] += 1
+
+    total = 0
+    total += sku_data["C"] * 20
+    total += sku_data["D"] * 15
+
+    
+
+
+
+
     
 
     
+
+    
+
 
